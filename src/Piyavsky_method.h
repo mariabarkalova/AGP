c7618366f41cda39fb3ec269badedf5e38418afb
Maria Barkalova <maria.k.barkalova@gmail.com>
2025-10-12 12:12:23 +0300

    Добавила Piyavsky_method.h

---
+++ src/Piyavsky_method.h
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <cmath>
+#include<vector>
+#include<algorithm>
+#include<iterator>
+#include <fstream>
+#include"Shekel/ShekelProblem.hpp"
+#include"Hill/HillProblem.hpp"
+using namespace std;
+
+class MethodPiyavsky {
+    double L;
+    double epsilon;
+    double a, b;
+    vector<double> x_values;
+    vector<double> z_values;
+    IOptProblem* problem;
+    //TShekelProblem* problem;
+    double min_x;
+    double min_value;
+    size_t point_count;
+    double true_opt_point;
+    double true_opt_value;
+    int flag;
+public:
+    double objective_function(double x)
+    {
+        return problem->ComputeFunction({ x });
+    }
+
+    double derivative(double x)
+    {
+        return problem->ComputeFunctionDerivatives({ x })[0];
+    }
+
+    double lipschitz_constant(double a, double b)
+    {
+        return problem->GetLipschitzConstant();
+    }
+
+    MethodPiyavsky(double epsilon_, double a_ = 0, double b_ = 0, int function_number = 0, int flag_ = 0) : epsilon(epsilon_), a(a_), b(b_), flag(flag_)
+    {
+        if (flag == 0)
+        {
+            problem = new TShekelProblem(function_number);
+        }
+        else
+            problem = new THillProblem(function_number);
+
+        if (a == 0 && b == 0)
+        {
+            vector<double> lb;
+            vector<double> ub;
+            problem->GetBounds(lb, ub);
+            a = lb[0];
+            b = ub[0];
+        }
+        L = lipschitz_constant(a, b);
+        x_values.push_back(a);
+        x_values.push_back(b);
+
+        z_values.push_back(objective_function(a));
+        z_values.push_back(objective_function(b));
+
+        vector<double> true_point = problem->GetOptimumPoint();
+        true_opt_point = true_point[0];
+        true_opt_value = problem->GetOptimumValue();
+    }
+
+    ~MethodPiyavsky()
+    {
+        delete problem;
+    }
+
+    void getResults(double& x_min, double& f_min, size_t& count, double& true_x, double& true_f)
+    {
+        x_min = min_x;
+        f_min = min_value;
+        count = point_count;
+        true_x = true_opt_point;
+        true_f = true_opt_value;
+    }
+
+    void algorithm()
+    {
+        while (true)
+        {
+            //Для (xi-1, xi) вычиcл R(i)
+            vector<double> R_values;
+            for (size_t i = 1; i < x_values.size(); ++i)
+            {
+                double delta = x_values[i] - x_values[i - 1];
+                double R = 0.5 * L * delta - 0.5 * (z_values[i] + z_values[i - 1]);
+                R_values.push_back(R);
+            }
+
+            double max_val = R_values[0];
+            size_t t = 0;
+            for (size_t i = 0;i < R_values.size(); i++)
+            {
+                if (R_values[i] > max_val)
+                {
+                    max_val = R_values[i];
+                    t = i;
+                }
+            }
+
+            double x_k1 = (x_values[t] + x_values[t + 1]) / 2.0 - (z_values[t + 1] - z_values[t]) / (2.0 * L);
+            //double x_k1 = (x_values[t] + x_values[t - 1]) / 2.0 - (z_values[t] - z_values[t-1]) / (2.0 * L);
+            double z_k1 = objective_function(x_k1);    // Зн-е функции в новой точке
+
+            // Вставляем новую точку и значение функции в вектора
+            x_values.insert(x_values.begin() + t + 1, x_k1);
+            sort(x_values.begin(), x_values.end());
+
+            z_values.insert(z_values.begin() + t + 1, z_k1);
+
+            //условие остановки
+            if (x_values[t + 1] - x_values[t] <= epsilon)
+            {
+                size_t min_index = 0;
+                min_value = z_values[0];
+                for (size_t i = 1; i < z_values.size(); i++)
+                {
+                    if (z_values[i] < min_value) {
+                        min_value = z_values[i];
+                        min_index = i;
+                    }
+                }
+                min_x = x_values[min_index];
+                point_count = x_values.size();
+
+                /*double min_x = x_values[min_index];
+                cout << "Точка минимума x = " << min_x << endl;
+                cout << "Кол-во точек: " << x_values.size() << endl;
+                cout << "Значение функции в точке: " << min_value << endl;
+
+                vector<double> true_opt_point = problem->GetOptimumPoint();
+                double true_opt_value = problem->GetOptimumValue();
+                cout << "Истинная точка минимума: " << true_opt_point[0] << endl;
+                cout << "Истинное значение функции: " << true_opt_value << endl;
+                cout << "Погрешность по x: " << abs(min_x - true_opt_point[0]) << endl;
+                cout << "Погрешность по f: " << abs(min_value - true_opt_value) << endl;
+                */
+                break;
+            }
+        }
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,100 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include"Piyavsky_method.h"
+using namespace std;
+
+void processAllFunctions(double a, double b, double epsilon, ofstream& file)
+{
+    file << "Номер ф-ции 1; Точка минимума 1; Знач ф-ции 1; Кол-во точек 1; Ист.знач.минимума 1; Ист.знач.ф-ции 1; Погрешность по x 1;Погрешность по f 1;"
+        << "Номер ф-ции 2; Точка минимума 2; Знач ф-ции 2; Кол-во точек 2; Ист.знач.минимума 2; Ист.знач.ф-ции 2; Погрешность по x 2;Погрешность по f 2"
+        << endl;
+    double min_x1, min_value1;
+    size_t point_count1;
+    double true_x1, true_f1;
+    double min_x2, min_value2;
+    size_t point_count2;
+    double true_x2, true_f2;
+
+    for (int function_number = 0; function_number < 999; function_number++)
+    {
+        // ф-я 1 (Shekel)
+        MethodPiyavsky method1(epsilon, a, b, function_number, 0);
+        method1.algorithm();
+        method1.getResults(min_x1, min_value1, point_count1, true_x1, true_f1);
+        double error_x1 = abs(min_x1 - true_x1);
+        double error_f1 = abs(min_value1 - true_f1);
+        file << function_number << ";" << min_x1 << ";" << min_value1 << ";" << point_count1 << ";" << true_x1 << ";" << true_f1 << ";" << error_x1 << ";" << error_f1 << ";";
+
+        // ф-я 2 (Hill)
+        MethodPiyavsky method2(epsilon, a, b, function_number, 1);
+        method2.algorithm();
+        method2.getResults(min_x2, min_value2, point_count2, true_x2, true_f2);
+        double error_x2 = abs(min_x2 - true_x2);
+        double error_f2 = abs(min_value2 - true_f2);
+        file << function_number << ";" << min_x2 << ";" << min_value2 << ";" << point_count2 << ";" << true_x2 << ";" << true_f2 << ";" << error_x2 << ";" << error_f2 << endl;
+    }
+}
+
 
 int main()
 {
-	std::cout << "Hello" << std::endl;
-	return 0;
+    setlocale(LC_ALL, "Russian");
+    double a, b;
+    double epsilon = 0.01;
+    cout << "Введите левую границу интервала (a): ";
+    cin >> a;
+    cout << "Введите правую границу интервала (b): ";
+    cin >> b;
+
+    ofstream file("results.csv");
+    if (!file.is_open())
+    {
+        cout << "Ошибка с файлом";
+        return 1;
+    }
+    /*file << "Номер ф-ции; Точка минимума; Знач ф-ции; Кол-во точек; Ист.знач.минимума; Ист.знач.ф-ции; Погрешность по x;Погрешность по f " << endl;
+    for(flag=0;flag<2;flag++)
+    {
+        for (function_number = 0;function_number < 999;function_number++)
+        {
+            MethodPiyavsky method(epsilon, a, b, function_number, flag);
+            method.algorithm();
+            method.getResults(min_x, min_value, point_count, true_x, true_f);
+            double error_x = abs(min_x - true_x);
+            double error_f = abs(min_value - true_f);
+            file << function_number << ";" << min_x << ";" << min_value << ";" << point_count << ";" << true_x << ";" << true_f << ";" << error_x << ";" << error_f << endl;
+        }
+    }*/
+
+    /*ofstream file_shekel("results_shekel.csv");
+    ofstream file_hill("results_hill.csv");
+
+    if (!file_shekel.is_open() || !file_hill.is_open())
+    {
+        cout << "Ошибка с файлами";
+        return 1;
+    }
+    file_shekel << "Номер_функции;Точка_минимума;Значение_функции;"
+        << "Количество_точек;Истинная_точка;Истинное_значение;"
+        << "Погрешность_x;Погрешность_f" << endl;
+
+    file_hill << "Номер_функции;Точка_минимума;Значение_функции;"
+        << "Количество_точек;Истинная_точка;Истинное_значение;"
+        << "Погрешность_x;Погрешность_f" << endl;
+
+    processFunctions(0, a, b, epsilon, file_shekel, "Shekel");
+    processFunctions(1, a, b, epsilon, file_hill, "Hill");
+
+    file_shekel.close();
+    file_hill.close();*/
+
+    processAllFunctions(a, b, epsilon, file);
+
+    file.close();
+    cout << "Файл успешно создан и заполнен";
+    return 0;
 }
+
+
+
